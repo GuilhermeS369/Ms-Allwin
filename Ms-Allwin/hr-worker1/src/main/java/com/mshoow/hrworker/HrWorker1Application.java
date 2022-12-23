da46f40a3fee2d0e79304afa202d3153d033084d
Guilherme Santos <guilherme460@yahoo.com.br>
2022-12-23 09:35:20 -0300

    Project hr-worker created

---
+++ Ms-Allwin/hr-worker1/src/main/java/com/mshoow/hrworker/HrWorker1Application.java
@@ -0,0 +1,13 @@
+package com.mshoow.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorker1Application.class, args);
+	}
+
+}
---
+++ Ms-Allwin/hr-worker1/src/test/java/com/mshoow/hrworker/HrWorker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshoow.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
