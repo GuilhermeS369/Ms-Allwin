a659cf240c711c15e219263670b9ff22fd790224
Guilherme Santos <guilherme460@yahoo.com.br>
2022-12-24 13:55:16 -0300

    hr-payroll implementation

---
+++ Ms-Allwin/hr-payroll/src/test/java/com/mshoow/hrpayroll/HrWorker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshoow.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
