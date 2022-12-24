24855d63b0515a234437da475a7d7ceef18a7c41
Guilherme Santos <guilherme460@yahoo.com.br>
2022-12-23 22:56:53 -0300

    hr-worker implementation

---
+++ Ms-Allwin/hr-worker1/src/main/java/com/mshoow/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.mshoow.hrworker.repositories;
+
+import com.mshoow.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
