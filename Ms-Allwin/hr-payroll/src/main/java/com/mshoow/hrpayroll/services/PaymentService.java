7d3b0aea96481f887029445c5ffd6e85457e9146
Guilherme Santos <guilherme460@yahoo.com.br>
2023-01-07 18:02:26 -0300

    RestTemplate

--- Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/services/PaymentService.java
+++ Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/services/PaymentService.java
@@ -1,12 +1,33 @@
 package com.mshoow.hrpayroll.services;
 
 import com.mshoow.hrpayroll.entities.Payment;
+import com.mshoow.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        //ARMAZENA O NOME DO PARAMETRO E O PARAMETRO
+        Map<String, String> uriVariables = new HashMap<>();
+        //SALVANDO AS DUAS INFORMAÇÕES NO MAP
+        uriVariables.put("id",""+workerId);
+        //FAZ A REQUISIÇÃO COM A URL PRONTA PRA RECEBER O ID, DEFINI A CLASSE A SER TRATADA E RECEBE A LISTA DE PARAMETROS
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 
