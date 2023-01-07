992124879e90fdc48dba41b24c52981f00099d90
Guilherme Santos <guilherme460@yahoo.com.br>
2023-01-07 18:35:48 -0300

    FeignClient

--- Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/services/PaymentService.java
+++ Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.mshoow.hrpayroll.services;
 
 import com.mshoow.hrpayroll.entities.Payment;
 import com.mshoow.hrpayroll.entities.Worker;
+import com.mshoow.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,23 +14,15 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        //ARMAZENA O NOME DO PARAMETRO E O PARAMETRO
-        Map<String, String> uriVariables = new HashMap<>();
-        //SALVANDO AS DUAS INFORMAÇÕES NO MAP
-        uriVariables.put("id",""+workerId);
-        //FAZ A REQUISIÇÃO COM A URL PRONTA PRA RECEBER O ID, DEFINI A CLASSE A SER TRATADA E RECEBE A LISTA DE PARAMETROS
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,uriVariables);
+
+        //Atribui ao worker a entidade chamada pelo método Feign
+        Worker worker = workerFeignClient.findById(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
-
-
 
 }
