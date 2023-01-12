5e352d3efe8a94c0cddb6b0d6a559a7b71384447
Guilherme Santos <guilherme460@yahoo.com.br>
2023-01-12 00:16:43 -0300

    Correção de dependências dos 3 projetos e anotação extra.

--- Ms-Allwin/hr-worker1/src/main/java/com/mshoow/hrworker/HrWorker1Application.java
+++ Ms-Allwin/hr-worker1/src/main/java/com/mshoow/hrworker/HrWorker1Application.java
@@ -2,7 +2,9 @@ package com.mshoow.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorker1Application {
 
