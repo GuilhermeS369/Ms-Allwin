0c400ba31aeb2c3d78fa24d1c7ac9414686eda4e
Guilherme Santos <guilherme460@yahoo.com.br>
2023-01-23 20:44:03 -0300

    Hystrix fault tolerance

--- Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/resources/PaymentResource.java
+++ Ms-Allwin/hr-payroll/src/main/java/com/mshoow/hrpayroll/resources/PaymentResource.java
@@ -2,12 +2,14 @@ package com.mshoow.hrpayroll.resources;
 
 import com.mshoow.hrpayroll.entities.Payment;
 import com.mshoow.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -16,13 +18,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand (fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
-
     }
 
-
+    public ResponseEntity<Payment> getPaymentAlternative (Long workerId,Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
 
 }
