46de4ede570cd72c7775dd4296612981b3e16a1a
Guilherme Santos <guilherme460@yahoo.com.br>
2023-01-23 20:50:17 -0300

    Project hr-api-gateway-zuul created

---
+++ Ms-Allwin/hr-api-gateway-zuul/src/main/java/com/mshoow/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.mshoow.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
