6774863f10035e6f406433ebaf8680d823e3dae8
Guilherme Santos <guilherme460@yahoo.com.br>
2023-03-29 12:21:09 -0300

    Hr-worker as a config server client.

--- Ms-Allwin/hr-worker/src/main/java/com/mshoow/hrworker/resources/WorkerResource.java
+++ Ms-Allwin/hr-worker/src/main/java/com/mshoow/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.mshoow.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
@@ -33,7 +37,7 @@ public class WorkerResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
+    public ResponseEntity<Object> findById(@PathVariable Long id){
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
@@ -41,5 +45,23 @@ public class WorkerResource {
         return ResponseEntity.ok(obj);
     }
 
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
