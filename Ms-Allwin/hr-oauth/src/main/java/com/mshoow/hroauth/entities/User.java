b90018247396978a75e1667e255d551a535f2ac4
Guilherme Santos <guilherme460@yahoo.com.br>
2023-05-30 19:03:17 -0300

    Login e geração do Token JWT

--- Ms-Allwin/hr-oauth/src/main/java/com/mshoow/hroauth/entities/User.java
+++ Ms-Allwin/hr-oauth/src/main/java/com/mshoow/hroauth/entities/User.java
@@ -1,12 +1,18 @@
 package com.mshoow.hroauth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     private long id;
@@ -52,11 +58,7 @@ public class User implements Serializable {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
+   public void setPassword(String password) {
         this.password = password;
     }
 
@@ -75,5 +77,39 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- Ms-Allwin/hr-oauth/src/main/java/com/mshoow/hroauth/services/UserService.java
+++ Ms-Allwin/hr-oauth/src/main/java/com/mshoow/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.mshoow.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
@@ -24,6 +27,19 @@ public class UserService {
         }
 
         logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+
+        if(user == null){
+            logger.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        logger.info("Email found: " + username);
         return user;
     }
 }
